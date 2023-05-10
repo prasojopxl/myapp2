7496d859c15cc8948f6f4f5d46b633f61cd82a0d
Sapto Prasojo <prasojodesigner@gmail.com>
2023-05-10 20:31:31 +0700

    first commit nextjs 13

---
+++ app/about/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/header'
+import Title from './title'
+
+export default function PageAbout() {
+  return (
+    <div>
+      <Header/>
+      <Title/>
+      PageAbout
+
+    </div>
+  )
+}
