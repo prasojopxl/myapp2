8d1cacd205cfa59397f86858db055315fa66f0b4
Sapto Prasojo <prasojodesigner@gmail.com>
2023-05-12 09:43:31 +0700

    add title, rev header, add images

--- app/components/header.js
+++ app/components/header.js
@@ -1,7 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header 2</div>
+    <div>
+      <Image src="/images/cover.png" width={200} height={200}/>
+      {/* <img src="/cover.png" width={100} height={100}/> */}
+    </div>
   )
 }
