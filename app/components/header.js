a16f5e5668d1b39d8258dcbe741970b04590dd0a
Sapto Prasojo <prasojodesigner@gmail.com>
2023-05-12 21:44:58 +0700

    add menu

--- app/components/header.js
+++ app/components/header.js
@@ -1,12 +1,24 @@
 import Image from 'next/image'
 import React from 'react'
 import "./style.css";
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <div className="header">
       <div className="container">
-        <Image src="/images/logo.png" width={130} height={17}/>
+        <div className="left">
+          <Image src="/images/logo.png" width={130} height={17}/>
+          <ul className="mainmenu">
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/">Textbook</Link></li>
+            <li><Link href="/">Statistics</Link></li>
+            <li><Link href="/">Games</Link></li>
+          </ul>
+        </div>
+        <div className="right">
+          kanan
+        </div>
       </div>
     </div>
   )
--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 import Header from './components/header'
 
 export default function Home() {
