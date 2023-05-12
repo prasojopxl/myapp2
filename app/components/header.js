9b21703cb1fc84da6aad6573972a86c96bebd8a3
Sapto Prasojo <prasojodesigner@gmail.com>
2023-05-12 20:28:49 +0700

    add header

--- app/components/header.js
+++ app/components/header.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import React from 'react'
+import "./style.css";
 
 export default function Header() {
   return (
-    <div>
-      <Image src="/images/cover.png" width={200} height={200}/>
-      {/* <img src="/cover.png" width={100} height={100}/> */}
+    <div className="header">
+      <div className="container">
+        <Image src="/images/logo.png" width={130} height={17}/>
+      </div>
     </div>
   )
 }
